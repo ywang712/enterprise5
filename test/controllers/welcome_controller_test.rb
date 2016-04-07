73d62dd517c0384e0cdd160af56bf6eb3c09ae3f
Yong Wang <ywang712@gmail.com>
2016-04-07 04:15:21 +0000

    built out navigation

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get features" do
+    get :features
+    assert_response :success
+  end
+
+  test "should get faqs" do
+    get :faqs
+    assert_response :success
+  end
+
+  test "should get pricing" do
+    get :pricing
+    assert_response :success
+  end
+
+end
